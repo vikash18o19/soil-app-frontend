c0c979f7c6edf6ff97625aa45563648c6f15082f
Vikash <vikash0cv@gmail.com>
2023-03-08 23:36:33 +0530

    added feature to send image to backend

--- lib/pages/image_pick.dart
+++ lib/pages/image_pick.dart
@@ -1,6 +1,12 @@
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+String url =
+    'https://ddb4-2401-4900-1ca9-da76-78d2-7839-c6f8-e1d0.in.ngrok.io/predict';
 
 class ImgPicker extends StatefulWidget {
   const ImgPicker({Key? key}) : super(key: key);
@@ -12,13 +18,15 @@ class ImgPicker extends StatefulWidget {
 class _ImgPicker extends State<ImgPicker> {
   File? image;
   List<XFile> multipleimage = [];
-
+  String prediction = "upload to get prediction";
+  var Status = 0;
   Future<void> pickimagefromgallery() async {
     final imagepicked = await ImagePicker().pickImage(
       source: ImageSource.gallery,
     );
     if (imagepicked != null) {
       setState(() {
+        Status = 0;
         image = File(imagepicked.path);
       });
     }
@@ -29,6 +37,7 @@ class _ImgPicker extends State<ImgPicker> {
         await ImagePicker().pickImage(source: ImageSource.camera);
     if (imagepicked != null) {
       setState(() {
+        Status = 0;
         image = File(imagepicked.path);
       });
     }
@@ -37,8 +46,27 @@ class _ImgPicker extends State<ImgPicker> {
   Future<void> pickmultipleimage() async {
     List<XFile> imagepicked = await ImagePicker().pickMultiImage();
     setState(() {
+      Status = 0;
       multipleimage.addAll(imagepicked);
     });
+  }
+
+  Future<String> sendImageToServer(File imageFile, String url) async {
+    final request = http.MultipartRequest('POST', Uri.parse(url));
+    final image = await http.MultipartFile.fromPath('image', imageFile.path);
+
+    request.files.add(image);
+    print("initiating request..");
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      setState(() {
+        Status = 200;
+      });
+      return response.stream.bytesToString();
+    } else {
+      throw Exception('Failed to upload image');
+    }
   }
 
   @override
@@ -65,6 +93,10 @@ class _ImgPicker extends State<ImgPicker> {
                     width: 300,
                     fit: BoxFit.contain,
                   ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(prediction),
             const SizedBox(
               height: 30,
             ),
@@ -102,23 +134,45 @@ class _ImgPicker extends State<ImgPicker> {
             const SizedBox(
               height: 10,
             ),
-            ElevatedButton.icon(
-                onPressed: () {
-                  multipleimage.removeRange(0, multipleimage.length);
-                  pickmultipleimage();
-                },
-                style: const ButtonStyle(
-                    minimumSize: MaterialStatePropertyAll(Size(220, 40)),
-                    backgroundColor: MaterialStatePropertyAll(Colors.brown)),
-                icon: SizedBox.square(
-                  dimension: 35,
-                  child: Icon(Icons.image_aspect_ratio_rounded),
-                ),
-                label: const Text(
-                  'Pick Multiple Image',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                )),
-            const SizedBox(
+            image != null && Status != 200
+                ? ElevatedButton.icon(
+                    onPressed: () {
+                      print("clicked");
+                      if (image != null) {
+                        print("image present");
+                      } else {
+                        print("image not present");
+                      }
+                      sendImageToServer(image!, url).then((response) {
+                        // Handle the response from the server
+
+                        print('Response: $response');
+                        setState(() {
+                          prediction = response;
+                        });
+                      }).catchError((error) {
+                        print("error occured!!");
+                        // Handle any errors that occur
+                        print('Error: $error');
+                      });
+                    },
+                    style: const ButtonStyle(
+                        minimumSize: MaterialStatePropertyAll(Size(220, 40)),
+                        backgroundColor:
+                            MaterialStatePropertyAll(Colors.brown)),
+                    icon: SizedBox.square(
+                      dimension: 35,
+                      child: Icon(Icons.upload_file_rounded),
+                    ),
+                    label: const Text(
+                      'Upload Image',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                    ))
+                : const SizedBox(
+                    height: 30,
+                  ),
+            SizedBox(
               height: 30,
             ),
             ConstrainedBox(
