2838480dd4389b98c8fafeb5489157c3729bf70a
Vikash <vikash0cv@gmail.com>
2023-04-09 23:20:27 +0530

    UI changes and some fixes for forgot password page

--- lib/pages/forgotpassword_page.dart
+++ lib/pages/forgotpassword_page.dart
@@ -17,7 +17,6 @@ class ForgotpasswordPage extends StatefulWidget {
 }
 
 class _ForgotpasswordPageState extends State<ForgotpasswordPage> {
-
   final mailidController = TextEditingController();
   final codeController = TextEditingController();
   final passController = TextEditingController();
@@ -29,8 +28,7 @@ class _ForgotpasswordPageState extends State<ForgotpasswordPage> {
   void _onTapDown() {
     setState(() {
       isTapped = true;
-    }
-    );
+    });
   }
 
   void _onTapUp() {
@@ -44,7 +42,7 @@ class _ForgotpasswordPageState extends State<ForgotpasswordPage> {
       isTapped = false;
     });
   }
-  
+
   Future<void> sendMail(BuildContext context, String email) async {
     final url = Uri.parse(
         'https://soil-app-backend.azurewebsites.net/user/forgot-password'); // replace with your API URL
@@ -61,9 +59,8 @@ class _ForgotpasswordPageState extends State<ForgotpasswordPage> {
 
     if (response.statusCode == 200) {
       setState(() {
-                          isTokenSent = true;
-                  });
-      
+        isTokenSent = true;
+      });
     } else {
       final error = json.decode(response.body)['message'];
       showDialog(
@@ -82,7 +79,8 @@ class _ForgotpasswordPageState extends State<ForgotpasswordPage> {
     }
   }
 
-  Future<void> resetPassword(BuildContext context, String token, String password, String repassword) async {
+  Future<void> resetPassword(BuildContext context, String token,
+      String password, String repassword) async {
     final url = Uri.parse(
         'https://soil-app-backend.azurewebsites.net/user/reset-password'); // replace with your API URL
 
@@ -92,20 +90,37 @@ class _ForgotpasswordPageState extends State<ForgotpasswordPage> {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       body: {
-        'passwordResetToken': token,
+        'token': token,
         'password': password,
-        'rePassword': repassword
+        'confirmPassword': repassword
       },
     );
 
     if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final message = data['message'];
       setState(() {
-              isTokenSent = false;
+        isTokenSent = false;
       });
-      Navigator.pushAndRemoveUntil(
-        context,
-        MaterialPageRoute(builder: (context) => LoginPage()),
-        (Route<dynamic> route) => false,
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Password changed!'),
+          content: Text("$message. Enter new login details after pressing OK"),
+          actions: [
+            TextButton(
+              onPressed: () => {
+                Navigator.pop(context),
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                  (Route<dynamic> route) => false,
+                )
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
       );
     } else {
       final error = json.decode(response.body)['message'];
@@ -116,7 +131,9 @@ class _ForgotpasswordPageState extends State<ForgotpasswordPage> {
           content: Text(error),
           actions: [
             TextButton(
-              onPressed: () => Navigator.pop(context),
+              onPressed: () => {
+                Navigator.pop(context),
+              },
               child: const Text('OK'),
             ),
           ],
@@ -125,165 +142,159 @@ class _ForgotpasswordPageState extends State<ForgotpasswordPage> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       backgroundColor: Colors.transparent,
       body: Container(
+        height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
               AppColors.c1,
               AppColors.c2,
               AppColors.c3,
               AppColors.c4,
               AppColors.c5,
-            ]
-          )
-        ),
-        child: isTokenSent==false? Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            AppBar2(),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 20),
-              child: Text(
-                'Please enter your email address to receive the mail for changing password:',
-                
-                style: TextStyle(
-                  color: AppColors.c0,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'RobotoMono' 
+            ])),
+        child: isTokenSent == false
+            ? SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    AppBar2(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 80, horizontal: 20),
+                      child: Text(
+                        'Please enter your email address to receive the mail for changing password:',
+                        style: TextStyle(
+                            color: AppColors.c0,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'RobotoMono'),
+                      ),
+                    ),
+                    SizedBox(height: 1),
+                    SizedBox(
+                        height: 50,
+                        width: 400,
+                        child: MyTextField(
+                            controller: mailidController,
+                            hintText: 'e-mail id',
+                            obscureText: false)),
+                    SizedBox(height: 25),
+                    GestureDetector(
+                      onTapDown: (_) => _onTapDown(),
+                      onTapUp: (_) => _onTapUp(),
+                      onTapCancel: () => _onTapCancel(),
+                      child: ElevatedButton(
+                          onPressed: () =>
+                              {sendMail(context, mailidController.text)},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: isTapped
+                                ? AppColors.c0.withOpacity(0.3)
+                                : Colors.transparent,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 120, vertical: 12),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                side: BorderSide(
+                                  color: AppColors.c0,
+                                  width: 1.0,
+                                )),
+                          ),
+                          child: Text(
+                            'Send mail',
+                            style: TextStyle(
+                              color: AppColors.c0,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )),
+                    )
+                  ],
                 ),
-            
+              )
+            : SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    AppBar2(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 80, horizontal: 20),
+                      child: Text(
+                        'Please enter the code sent on the mail and reset your password:',
+                        style: TextStyle(
+                            color: AppColors.c0,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'RobotoMono'),
+                      ),
+                    ),
+                    SizedBox(height: 1),
+                    SizedBox(
+                        height: 50,
+                        width: 400,
+                        child: MyTextField(
+                            controller: codeController,
+                            hintText: 'code',
+                            obscureText: false)),
+                    SizedBox(height: 25),
+                    SizedBox(
+                        height: 50,
+                        width: 400,
+                        child: MyTextField(
+                            controller: passController,
+                            hintText: 'password',
+                            obscureText: true)),
+                    SizedBox(height: 25),
+                    SizedBox(
+                        height: 50,
+                        width: 400,
+                        child: MyTextField(
+                            controller: repassController,
+                            hintText: 're-enter password',
+                            obscureText: true)),
+                    SizedBox(height: 50),
+                    GestureDetector(
+                      onTapDown: (_) => _onTapDown(),
+                      onTapUp: (_) => _onTapUp(),
+                      onTapCancel: () => _onTapCancel(),
+                      child: ElevatedButton(
+                          onPressed: () => {
+                                resetPassword(context, codeController.text,
+                                    passController.text, repassController.text)
+                              },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: isTapped
+                                ? AppColors.c0.withOpacity(0.3)
+                                : Colors.transparent,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 70, vertical: 12),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                side: BorderSide(
+                                  color: AppColors.c0,
+                                  width: 1.0,
+                                )),
+                          ),
+                          child: Text(
+                            'Reset password',
+                            style: TextStyle(
+                              color: AppColors.c0,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )),
+                    )
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 1
-            ),
-            SizedBox(
-              height:50,
-              width: 400,
-              child: MyTextField(
-                controller: mailidController, 
-                hintText: 'e-mail id', 
-                obscureText: false
-              )
-            ),
-            SizedBox(
-              height: 25
-            ),
-            GestureDetector(
-              onTapDown: (_) => _onTapDown(),
-              onTapUp: (_) => _onTapUp(),
-              onTapCancel: () => _onTapCancel(),
-              child: ElevatedButton(
-                onPressed:() => {
-                  sendMail(context, mailidController.text)
-                }, 
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: isTapped? AppColors.c0.withOpacity(0.3) : Colors.transparent,
-                  padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 12),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100), side: BorderSide(color: AppColors.c0, width: 1.0,) ),
-                ),
-                child: Text(
-                  'Send mail',
-                  style: TextStyle(
-                    color: AppColors.c0,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )),
-            )
-          ],
-        ):Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AppBar2(),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 20),
-              child: Text(
-                'Please enter the code sent on the mail and reset your password:',
-                
-                style: TextStyle(
-                  color: AppColors.c0,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'RobotoMono' 
-                ),
-            
-              ),
-            ),
-            SizedBox(
-              height: 1
-            ),
-            SizedBox(
-              height:50,
-              width: 400,
-              child: MyTextField(
-                controller: codeController, 
-                hintText: 'code', 
-                obscureText: false
-              )
-            ),
-            SizedBox(
-              height: 25
-            ),
-            SizedBox(
-              height:50,
-              width: 400,
-              child: MyTextField(
-                controller: passController, 
-                hintText: 'password', 
-                obscureText: true
-              )
-            ),
-            SizedBox(
-              height: 25
-            ),
-            SizedBox(
-              height:50,
-              width: 400,
-              child: MyTextField(
-                controller: repassController, 
-                hintText: 're-enter password', 
-                obscureText: true
-              )
-            ),
-            SizedBox(
-              height: 25
-            ),
-            GestureDetector(
-              onTapDown: (_) => _onTapDown(),
-              onTapUp: (_) => _onTapUp(),
-              onTapCancel: () => _onTapCancel(),
-              child: ElevatedButton(
-                onPressed:() => {
-                  resetPassword(context, codeController.text, passController.text, repassController.text)
-                }, 
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: isTapped? AppColors.c0.withOpacity(0.3) : Colors.transparent,
-                  padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 12),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100), side: BorderSide(color: AppColors.c0, width: 1.0,) ),
-                ),
-                child: Text(
-                  'Reset password',
-                  style: TextStyle(
-                    color: AppColors.c0,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )),
-            )
-
-          ],
-        ),
       ),
     );
   }
